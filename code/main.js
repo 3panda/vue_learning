c0847ebb5bb918ab1f0ca8934b60b09fd0eeb6c7
3panda <3panda@gmail.com>
2018-09-02 15:39:20 +0900

    外部jsonの読み込み　p84

--- code/main.js
+++ code/main.js
@@ -1,33 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
-        list: [
-            {id: 1, name: 'スライム', hp: 100 },
-            {id: 2, name: 'ゴブリン', hp: 200 },
-            {id: 3, name: 'ドラゴン', hp: 500 }
-        ]
-    },
-    methods: {
-        //
-        doAdd: function () {
-            var max = this.list.reduce(function (a, b) {
-                return a > b.id ? a : b.id
-            }, 0)
-            //
-            this.list.push({
-                id: max + 1,
-                name: this.name,
-                hp: 500
-            })
-        },
-        doRemove : function (index) {
-            //
-            this.list.splice(index, 1);
-        },
-        doAttack: function (index) {
-            this.list[index].hp -= 10 // hpを減らす
-        }
-    }
+        list: []
+     },
+     created: function () {
+        axios.get('list.json').then(function (response) {
+          // 取得完了したらlistリストに代入
+          this.list = response.data
+        }.bind(this)).catch(function (e) {
+          console.error(e)
+        })
+      }
+      
   })
 
-  // console.log(app.count);
+  // console.log(app.count)
